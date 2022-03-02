af36c5181ed6b16f31317493840af5b4e219cccd
Tory Dannenberg <rfdberg@gmail.com>
2022-03-02 16:40:58 -0700

    Fix routing bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { RunningLogsComponent } from './running-logs/running-logs.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { RunDetailsComponent } from './run-details/run-details.component';
 
 const routes: Routes = [
   {
-    path: 'dashboard',
+    path: '',
     component: DashboardComponent
   },
   {
     path: 'runs',
     component: RunningLogsComponent
   },
-  // {
-  //   path: 'details/:id',
-  //   component: RunDetailsComponent
-  // },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
